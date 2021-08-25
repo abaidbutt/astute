083ed3aab2dbe783b5c8c9f1482cbd4d92cae970
abaidbutt <49604057+abaidbutt@users.noreply.github.com>
2021-08-25 10:42:24 +0500

    check tahta

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,10 @@ class MyDocument extends Document {
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
           <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
+          <meta
+            httpEquiv="Content-Security-Policy"
+            content="upgrade-insecure-requests"
+          />{" "}
           <script
             src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}`}
           ></script>
